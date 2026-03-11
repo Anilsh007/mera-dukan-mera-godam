fd44ae7bb2c20e1e52323957890965d12efbcd94
Anilsh007 <anil.sh101@gmail.com>
2026-03-11 22:52:17 +0530

    fix: explicit default export for settings

---
+++ app/dashboard/stock/page.js
@@ -0,0 +1,9 @@
+// सुनिश्चित करें कि 'export default' लिखा है और फंक्शन का नाम Capital letter से शुरू है
+export default function StockPage() {
+  return (
+    <div>
+      <h1>Stock Management</h1>
+      {/* आपका बाकी कोड यहाँ */}
+    </div>
+  );
+}
